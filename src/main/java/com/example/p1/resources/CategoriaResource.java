2eb4042e96e128cb92621cb9e8e20b73f4d10d46
n4co <nf.oliveira@hotmail.com>
2021-10-26 00:08:05 -0300

    Primeira classe categoria domain

--- src/main/java/com/example/p1/resources/CategoriaResource.java
+++ src/main/java/com/example/p1/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.example.p1.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.p1.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "listar está funcionando";
+	public List<Categoria> lista() {
+	 Categoria cat1 = new Categoria(1, "Informática");
+	 Categoria cat2 = new Categoria(2, "Escritório");
+
+	 List<Categoria> lista = new ArrayList<>();
+	 lista.add(cat1);
+	 lista.add(cat2);
+
+	 return lista;
+
 	}
+
 }
