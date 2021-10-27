f0b0b91b8908e5e2b027b96ffd118a9300b2de6c
n4co <nf.oliveira@hotmail.com>
2021-10-26 21:36:08 -0300

    Criando operação de instanciação

--- src/main/java/com/example/p1/P1Application.java
+++ src/main/java/com/example/p1/P1Application.java
@@ -1,13 +1,34 @@
 package com.example.p1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class P1Application {
+import com.example.p1.domain.Categoria;
+import com.example.p1.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class P1Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(P1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		Categoria cat1 = new Categoria(null, "Informaticá");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+	categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	
 	}
 
 }
