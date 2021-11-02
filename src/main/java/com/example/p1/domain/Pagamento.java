54d0a1607e36d7d02475ab58a6457a242f017e38
n4co <nf.oliveira@hotmail.com>
2021-11-01 23:17:14 -0300

    Inserindo uma nova categoria

--- src/main/java/com/example/p1/domain/Pagamento.java
+++ src/main/java/com/example/p1/domain/Pagamento.java
@@ -14,7 +14,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.example.p1.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
  @Entity
  @Inheritance(strategy=InheritanceType.JOINED)
@@ -26,7 +26,7 @@ public class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
